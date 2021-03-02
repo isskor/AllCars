f9e8ef92f73b54f20f0300686218621a725d9cb2
jerry <jphuong91@gmail.com>
2021-03-02 14:01:57 +0100

    added checkoutHandler in cart and checkoutForm state
    checkoutHandler return cart items and checkoutform

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -4,7 +4,7 @@ export const CartContext = createContext();
 const CartContextProvider = ({ children }) => {
   const { Provider } = CartContext;
   const [cart, setCart] = useState([]);
-
+  const [checkOutForm, setCheckOutForm] = useState({});
   const addToCart = (item) => {
     setCart([...cart, item]);
   };
@@ -13,12 +13,21 @@ const CartContextProvider = ({ children }) => {
     setCart(cart.filter((car) => car !== item));
   };
 
+  const handleCheckout = (form) => {
+    return {
+      cart,
+      checkOutForm,
+    };
+  };
+
   return (
     <Provider
       value={{
         cart,
         addToCart,
         removeFromCart,
+        setCheckOutForm,
+        handleCheckout,
       }}
     >
       {children}
