23b9b0b33db08ed760e5314cd6ad952987998f90
Mehmet Ziya <mehmetziya82@gmail.com>
2021-03-02 11:59:04 +0100

    created and import Navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import { BrowserRouter as Router, Route } from "react-router-dom" ;
-
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className='App'>
-      <h1>AllCars</h1>
+      <Router>
+          <h1>AllCars</h1>
+          <Navbar />
+          <Route exact path="/deals" ></Route>
+          <Route exact path="/" ></Route>
+          <Route exact path="/about" ></Route>
+          
+        
+      </Router>
+      
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+
+import { Link } from "react-router-dom"
+
+
+const Navbar = () => {
+  return (
+    <nav >
+      <Link to="/">Deals</Link>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      
+    </nav>
+  );
+}
+ 
+export default Navbar;
