12ca57675c240f14b92db6b5ea9b312f8e7d8a1f
jerry <jphuong91@gmail.com>
2021-03-02 14:10:30 +0100

    removed CartContext from this branch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import CarContextProvider from './contexts/CarContext';
 import Test from './components/test';
-import CartContextProvider from './contexts/CartContext';
+// import CartContextProvider from './contexts/CartContext';
 function App() {
   return (
     <CarContextProvider>
-      <CartContextProvider>
-        <div className='App'>
-          <h1>AllCars</h1>
-        </div>
-        <Test />
-      </CartContextProvider>
+      {/* <CartContextProvider> */}
+      <div className='App'>
+        <h1>AllCars</h1>
+      </div>
+      <Test />
+      {/* </CartContextProvider> */}
     </CarContextProvider>
   );
 }
