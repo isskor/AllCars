52a43d18f1f189f0479db77631f14c85bd8b0174
Pernilla <coolanilla@hotmail.com>
2021-03-02 16:11:51 +0100

    Mapped out CarCard in Home and added it to App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import FooterNavbar from './components/FooterNavbar';
 import Navbar from './components/Navbar';
 import CarContextProvider from './contexts/CarContext';
 import CartContextProvider from './contexts/CartContext';
-import Test from './components/test';
+import Home from './pages/Home';
+// import Test from './components/test';
 function App() {
   return (
     <div className='App'>
@@ -13,9 +14,9 @@ function App() {
             <FooterNavbar />
             <Navbar />
             <Route exact path='/deals'></Route>
-            <Route exact path='/'></Route>
+            <Route exact path='/' component={Home}></Route>
             <Route exact path='/about'></Route>
-            <Test />
+            {/* <Test /> */}
           </CartContextProvider>
         </CarContextProvider>
       </Router>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,21 @@
+import { useContext } from 'react';
+import { CarContext } from '../contexts/CarContext';
+import CarCard from '../components/CarCard';
+
 function Home() {
+
+    const { cars } = useContext(CarContext);
+
     return (
         <div className="home">
             <div className="hero">
                 <img src="/assets/background/allcars_home.jpg" />
                 <h1>Quality never goes out of style</h1>
+                <div className="carList">
+                    {cars.map((car, i) => (
+                        <CarCard key={i} car={car} />
+                    ))}
+                </div>
             </div>
         </div>
     )
