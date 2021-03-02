3f1f26faa9a788fe2c1c414d2f48f439c9c65c79
Zainab <zainab.tech@pm.me>
2021-03-02 20:13:15 +0100

    added about-hero-image div container

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,8 +6,10 @@ import aboutHero from '../../public/assets/background/allcars_about.jpg';
 const about = () => {
     return ( 
         <div className = "about-container">
-        
+            <div className="about-hero-image">
+               
             </div>
+           
         </div>
      );
 }
