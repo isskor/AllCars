79616f95285b7c2155cee425c1f4e93f14bff8ed
jerry <jphuong91@gmail.com>
2021-03-02 19:30:42 +0100

    prepare for useReducer in Car state

--- src/contexts/CarContext.js
+++ src/contexts/CarContext.js
@@ -1,12 +1,17 @@
-import { useState, createContext } from 'react';
+import { useState, createContext, useReducer } from 'react';
 import carsJSON from '../json/cars.json';
 
 const carList = carsJSON.map((car) => ({ ...car, discounted: false }));
 export const CarContext = createContext();
 
+// const searchReducer(state, action) => {
+
+// }
+
 const CarContextProvider = ({ children }) => {
   const { Provider } = CarContext;
   const [cars, setCars] = useState(carList);
+  // const [cars, dispatch] = useReducer(searchReducer, cars)
 
   return <Provider value={{ cars }}>{children}</Provider>;
 };
