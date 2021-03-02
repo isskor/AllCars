960bc3142d087797fffe172f9c8267cdcbb70a27
Pernilla <coolanilla@hotmail.com>
2021-03-02 14:22:16 +0100

    Added divs, p-tags and a button to CarCard

--- src/components/CarCard.js
+++ src/components/CarCard.js
@@ -2,7 +2,19 @@ function CarCard() {
 
     return (
         <div className="carCard">
-            <h1>CarCard works!</h1>
+            <div className="imgContainer">
+                <img src="/assets/car-pictures/Acura-NSX-2000.jpg" alt="Car picture" />
+                <p>Car price</p>
+                <div className="imgTextWrapper">
+                    <p>Car make</p>
+                    <p>Car year</p>
+                </div>
+            </div>
+            <div className="statsContainer">
+                <p>model</p>
+                <p>miles</p>
+                <button>Add to cart</button>
+            </div>
         </div>
     )
     
