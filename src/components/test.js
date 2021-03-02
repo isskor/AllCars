358ae4f44b2dab86be45f6ac37ae767d501f94ac
jerry <jphuong91@gmail.com>
2021-03-02 14:44:12 +0100

    added buttons to test in test.js

--- src/components/test.js
+++ src/components/test.js
@@ -1,22 +1,24 @@
 import { useContext } from 'react';
 import { CarContext } from '../contexts/CarContext';
-// import { CartContext } from '../contexts/CartContext';
+import { CartContext } from '../contexts/CartContext';
 const Test = () => {
   const { cars } = useContext(CarContext);
-  //   const { addToCart, cart } = useContext(CartContext);
+  const { addToCart, cart, removeFromCart } = useContext(CartContext);
   console.log(cars);
   return (
     <div>
       {/* for testing, display cart length */}
-      {/* <h2>cart items {cart.length}</h2> */}
+      <h2>cart items {cart.length}</h2>
       {cars.map((car) => (
         <div>
           {/* for testing, click on title to add to cart */}
-          {/* <h1 onClick={() => addToCart(car)}>{car.make}</h1> */}
+          <h1>{car.make}</h1>
           <p>{car.make}</p>
           <p>{car.model}</p>
           <p>{car.year}</p>
           <p>{`${car.discounted}`}</p>
+          <button onClick={() => addToCart(car)}>add to cart</button>
+          <button onClick={() => removeFromCart(car)}>remove</button>
         </div>
       ))}
     </div>
