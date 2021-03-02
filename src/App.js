011de9851bd04989b55e8ba2a9537c199fa9c8d3
jerry <jphuong91@gmail.com>
2021-03-02 15:06:25 +0100

    fixed error after merging with dev

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import { BrowserRouter as Router, Route } from "react-router-dom" ;
-import FooterNavbar from "./components/FooterNavbar";
-import Navbar from "./components/Navbar"
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import FooterNavbar from './components/FooterNavbar';
+import Navbar from './components/Navbar';
 import CarContextProvider from './contexts/CarContext';
+import CartContextProvider from './contexts/CartContext';
 import Test from './components/test';
 function App() {
   return (
     <div className='App'>
       <Router>
-    <CarContextProvider>
-      {/* <CartContextProvider> */}
-      <div className='App'>
-      </div>
-          <FooterNavbar />
-          <Navbar />
-          <Route exact path="/deals" ></Route>
-          <Route exact path="/" ></Route>
-          <Route exact path="/about" ></Route>
-    </CarContextProvider>
-      {/* </CartContextProvider> */}
+        <CarContextProvider>
+          <CartContextProvider>
+            <FooterNavbar />
+            <Navbar />
+            <Route exact path='/deals'></Route>
+            <Route exact path='/'></Route>
+            <Route exact path='/about'></Route>
+            <Test />
+          </CartContextProvider>
+        </CarContextProvider>
       </Router>
-      <Test />
+    </div>
   );
 }
 
