4a8390a327d70e26db367dffb1593fb9bdcf911a
khaledh <hassan.khaled@hotmail.se>
2021-03-03 21:06:46 +0100

    moved form to CartForm

---
+++ src/components/CartForm.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from '../css/cart.module.css';
+
+const CartForm = () => {
+    return (
+        <div>
+            <h1>Checkout</h1>
+            <form >
+                <label>
+                    Name:<br />
+                    <input  type="text" className={styles.cart_input} />
+                </label> <br />
+                <label>
+                    Name:<br />
+                    <input  type="text" className={styles.cart_input} />
+                </label> <br />
+                <label>
+                    Name:<br />
+                    <input  type="text" className={styles.cart_input} />
+                </label>
+                <br />
+                <label>
+                    Name:<br />
+                    <input  type="text" className={styles.cart_input} />
+                </label>
+                <br />
+                <br />
+                <button> Purchase </button>
+            </form>
+        </div>
+    )
+}
+
+export default CartForm
