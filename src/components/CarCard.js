ff4d5db133f7fe8eba66c6053f0c4efbf01d6339
Pernilla <coolanilla@hotmail.com>
2021-03-03 12:01:30 +0100

    Added some styles to CarCard and added more divs and p-tags

--- src/components/CarCard.js
+++ src/components/CarCard.js
@@ -1,18 +1,26 @@
+import style from '../css/CarCard.module.css';
+
 function CarCard(prop) {
 
     return (
-        <div className="carCard">
-            <div className="imgContainer">
+        <div className={style.carCard}>
+            <div className={style.imgContainer}>
                 <img src={`/assets/car-pictures/${prop.car.make}-${prop.car.model}-${prop.car.year}.jpg`} alt="Car picture" />
-                <p>{prop.car.price}</p>
-                <div className="imgTextWrapper">
-                    <p>{prop.car.make}</p>
-                    <p>{prop.car.year}</p>
+                <p className={style.carPrice}>${prop.car.price}</p>
+                <div className={style.imgTextWrapper}>
+                    <p className={style.wrapperText}>{prop.car.make}</p>
+                    <p className={style.wrapperText}>{prop.car.year}</p>
                 </div>
             </div>
             <div className="statsContainer">
-                <p>{prop.car.model}</p>
-                <p>{prop.car.miles}</p>
+                <div className="carModel">
+                    <p>Model</p>
+                    <p>{prop.car.model}</p>
+                </div>
+                <div className="carMiles">
+                    <p>Miles</p>
+                    <p>{prop.car.miles}</p>
+                </div>
                 <button>Add to cart</button>
             </div>
         </div>
