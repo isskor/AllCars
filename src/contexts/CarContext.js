3a76e2bbeafd936c35e169f8ed1b51330d6b59a3
jerry <jphuong91@gmail.com>
2021-03-03 12:41:34 +0100

    added disctouned = true to the 3 first cars

--- src/contexts/CarContext.js
+++ src/contexts/CarContext.js
@@ -1,7 +1,20 @@
+import { Looks } from '@material-ui/icons';
 import { useState, createContext, useReducer } from 'react';
 import carsJSON from '../json/cars.json';
 
-const carList = carsJSON.map((car) => ({ ...car, discounted: false }));
+const carList = carsJSON.map((car, i) => {
+  if (i < 3) {
+    console.log(car);
+    return {
+      ...car,
+      discounted: true,
+    };
+  }
+  return {
+    ...car,
+    discounted: false,
+  };
+});
 export const CarContext = createContext();
 
 // const searchReducer(state, action) => {
@@ -12,6 +25,7 @@ const CarContextProvider = ({ children }) => {
   const { Provider } = CarContext;
   const [cars, setCars] = useState(carList);
   // const [cars, dispatch] = useReducer(searchReducer, cars)
+  console.log(cars);
 
   return <Provider value={{ cars }}>{children}</Provider>;
 };
