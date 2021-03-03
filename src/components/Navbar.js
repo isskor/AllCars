ca5814dc7424fba7bf5f0cdd18793c5233454973
Mehmet Ziya <mehmetziya82@gmail.com>
2021-03-03 12:49:04 +0100

    added style Navbar module css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
 
 import { Link } from "react-router-dom";
 import FooterNavbar from "./FooterNavbar"
-
+import styles from "../css/Navbar.module.css" ;
 
 
 
 const Navbar = () => {
   return (
     <div>
-       <nav >
-            <Link to="/"><span>AllCars</span></Link>
+       <nav className={styles.mainNavbar}>
+            <Link to="/"><span className={styles.logo}>AllCars</span></Link>
             <FooterNavbar />  
       </nav>   
     </div> 
