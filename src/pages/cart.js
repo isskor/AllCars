e30a98c3958c07f4acff4d7eaa515c64051bd0be
khaledh <hassan.khaled@hotmail.se>
2021-03-03 21:12:49 +0100

    created cart templets

---
+++ src/pages/cart.js
@@ -0,0 +1,25 @@
+
+import { useContext } from 'react';
+import { CartContext } from '../context/CartContext';
+import CartItem from './CartItem';
+import CartForm from '../components/CartForm';
+
+const Cart = () => {
+    const { cart } = useContext(CartContext);
+    const totalCost = cart.reduce((acc, cur) => acc + cur.price, 0);
+    return (
+        <div>
+            <ul>
+                {cart.map((cartItem) => (
+                    <CartItem cartItem={cartItem} key={cartItem.vin} />
+                ))}
+            </ul>
+            <div className='total'>
+                <h4>Total</h4>
+                <h3>{totalCost}​​​​​</h3>
+            </div>
+            <CartForm />
+        </div>);
+};
+
+export default Cart;
