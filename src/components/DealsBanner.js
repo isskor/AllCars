8d405be8ed0ae00e2eadb45a75ece80f9ff061dd
Johannes <polyspark.prod@gmail.com>
2021-03-03 14:55:35 +0100

    fixed header bg img

--- src/components/DealsBanner.js
+++ src/components/DealsBanner.js
@@ -3,6 +3,7 @@ import style from "../css/dealsBanner.module.css";
 const DealsBanner = () => {
   return (
     <div className={style.header}>
+      <img src="/assets/background/allcars_deals.jpg" alt=""/>
       <h1>Quality Deals</h1>
     </div>
   );
