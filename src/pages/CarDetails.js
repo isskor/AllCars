8c49e866e7d19ad2d9651cf0a3ccbd8fedb491b6
Zainab <zainab.tech@pm.me>
2021-03-03 21:44:35 +0100

    CarDetail. Imported styles. Added ClassNames. Reordered stats
    rearranging JSX before adding styling to match mockup

--- src/pages/CarDetails.js
+++ src/pages/CarDetails.js
@@ -2,6 +2,7 @@ import { useContext, useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { CarContext } from "../contexts/CarContext";
 import styles from  '../css/CarDetails.module.css'; 
+import AddToCartButton from '../components/AddToCart';
 
 
 
@@ -28,31 +29,41 @@ function CarDetails(props) {
   const renderCar = () => {
     return (
       <div className="car">
-        <div className="image-container">
-        <img src={car.image} alt={car.name} />
-        </div>
-        <div className="container">
-          <span className="make">{car.make}</span>
-          <div className="make row">
-            <span>Make: {car.make}</span>
-          </div>
-          <div className="model row">
-            <span>Model: {car.model}</span>
-          </div>
-          <div className="year row">
-            <span>Year {car.year}</span>
-          </div>
-          <div className="price row">
-            <span>Price {car.price}</span>
-          </div>
-          <div className="miles row">
-            <span>Miles {car.miles}</span>
-          </div>
-          <span className="long-desc">{car.longDesc}</span>
-        </div>
         <NavLink className="back" to="/">
           Back
         </NavLink>
+        <div className={styles.carImageContainer}>
+        <img src={car.image} alt={car.name} />
+        <div className={styles.cartButton}>
+            <button>{AddToCartButton}</button>
+        </div>
+        </div>
+        <div className={styles.carStatsContainer}>
+          <div className={styles.carDescriptionShort}>{car.descShort}</div>
+          <div className={styles.carPriceLabel}>{car.price}</div>
+          <div className={styles.carMake}>
+            <span>Make: {car.make}</span>
+          </div>
+          <div className={styles.carModel}>
+            <span>Model: {car.model}</span>
+          </div>
+          <div className={styles.carYear}>
+            <span>Year {car.year}</span>
+          </div>
+          <div className={styles.carMileage}>
+            <span>Mileage {car.miles}</span>
+          </div>
+          <div className={styles.carVIN}>
+            <span>VIN {car.VIN}</span>
+          </div>
+          <div className={styles.carCity}>
+            <span>City {car.city}</span>
+          </div>
+          <span className={styles.carLongDescription}>{car.longDesc}</span>
+        </div>
+        <div className={styles.cartButton}>
+        <button>{AddToCartButton}</button>
+        </div>
       </div>
     );
   };
