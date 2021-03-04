76e288cf95f32e5740fe20e1ffa74c809326c707
Johannes <polyspark.prod@gmail.com>
2021-03-04 10:42:08 +0100

    Hmm..

--- src/components/CarCard.js
+++ src/components/CarCard.js
@@ -1,25 +1,25 @@
 import style from '../css/CarCard.module.css';
 
-function CarCard(prop) {
+function CarCard({car}) {
 
     return (
         <div className={style.carCard}>
             <div className={style.imgContainer}>
-                <img src={`/assets/car-pictures/${prop.car.make}-${prop.car.model}-${prop.car.year}.jpg`} alt="Car picture" />
-                <p className={style.carPrice}>${prop.car.price}</p>
+                <img src={`/assets/car-pictures/${car.make}-${car.model}-${car.year}.jpg`} alt="Car picture" />
+                <p className={style.carPrice}>${car.price}</p>
                 <div className={style.imgTextWrapper}>
-                    <p className={style.wrapperText}>{prop.car.make}</p>
-                    <p className={style.wrapperText}>{prop.car.year}</p>
+                    <p className={style.wrapperText}>{car.make}</p>
+                    <p className={style.wrapperText}>{car.year}</p>
                 </div>
             </div>
             <div className={style.statsContainer}>
                 <div className="carModel">
                     <p className={style.model}>Model</p>
-                    <p>{prop.car.model}</p>
+                    <p>{car.model}</p>
                 </div>
                 <div className={style.milesContainer}>
                     <p className={style.miles}>Miles</p>
-                    <p>{prop.car.miles}</p>
+                    <p>{car.miles}</p>
                 </div>
                 <button className={style.addBtn}>Add to cart</button>
             </div>
