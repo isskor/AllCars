d337530b879fb5f798117822753c65e1210a4a73
Mehmet Ziya <mehmetziya82@gmail.com>
2021-03-04 10:44:51 +0100

    added switch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import CarContextProvider from './contexts/CarContext';
 import CartContextProvider from './contexts/CartContext';
-import Test from './components/test';
+
 function App() {
   return (
     <div className='App'>
@@ -10,13 +10,19 @@ function App() {
         <CarContextProvider>
           <CartContextProvider>
             <Navbar />
-            <Route path='/deals' />
-            <Route exact path='/' />
-            <Route path='/about' />
-            <Route exact path='/car/:id' />
-            <Route exact path='/cart' />
-            <Route path='/receipt' />
-            <Test />
+
+            <Switch>
+                <Route path='/deals' />
+                <Route exact path='/' />
+                <Route path='/about' />
+                <Route exact path='/car/:id' />
+                <Route exact path='/cart' />
+                <Route path='/receipt' />
+            </Switch>
+            
+            
+            
+
           </CartContextProvider>
         </CarContextProvider>
       </Router>
