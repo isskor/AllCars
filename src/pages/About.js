3307d2ec9f99de431fc6b633463aeecd8d1b5c9b
Zainab <zainab.tech@pm.me>
2021-03-04 12:48:49 +0100

    About. Added contact information
    Added JSX  <section>, <address> with contact information

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,7 @@ import styles from "../css/About.module.css";
 
 const About = () => {
     return (
-        <div className={styles.aboutContainer}>
+        <section className={styles.aboutContainer}>
             <div className={styles.aboutHeroContainer}>
                 <h1 className={styles.aboutTitle}>About us</h1>
                 <img className={styles.aboutHeroImage} src="/assets/background/allcars_about.jpg" alt="About Header" />
@@ -17,11 +17,21 @@ const About = () => {
                 <div className={styles.aboutImageStaff}>
                     <img className={styles.aboutTeam} src="/assets/background/allcars-team.jpg" alt="About the AllCars staff" />
                 </div>
-                <div className={styles.aboutContactInfo}>
-                    Contact us!
-                </div>
+                <section className={styles.aboutContactInfo}>
+                    <div className={styles.contactForm}></div>
+                    <div className={styles.contactInformation}>
+                        <article className={styles.customerContact}>
+                            <h2>Customer Service</h2>
+                            <p className="customerTimings">08:00-1730 Monday to Friday</p>
+                            <address className="customerEmail">
+                                <a href="mailto:info@allcars.com">info@allcars.com</a><br/>
+                                <a href="tel: +12345678901">(234)567-8901</a>
+                            </address>
+                        </article>
+                    </div>
+                </section>
             </div>
-        </div>
+        </section>
     );
 }
 
