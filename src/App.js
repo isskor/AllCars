685d0e92c3dfdc7a86aa271d8d00aba5709e266b
jerry <jphuong91@gmail.com>
2021-03-05 12:24:29 +0100

    added test component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import CarContextProvider from './contexts/CarContext';
 import CartContextProvider from './contexts/CartContext';
 import Home from './pages/Home';
-// import Test from './components/test';
+import Test from './components/test';
 function App() {
   return (
     <div className='App'>
@@ -11,13 +11,13 @@ function App() {
         <CarContextProvider>
           <CartContextProvider>
             <Navbar />
+            <Route exact path='/' component={Home}></Route>
             <Route path='/deals' />
             <Route path='/about' />
             <Route exact path='/car/:id' />
             <Route exact path='/cart' />
             <Route path='/receipt' />
             <Test />
-            <Route exact path='/' component={Home}></Route>
           </CartContextProvider>
         </CarContextProvider>
       </Router>
