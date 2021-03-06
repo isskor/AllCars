d409e4c9a595c163ded32fdaaf201992858ee8a5
jerry <jphuong91@gmail.com>
2021-03-06 20:23:51 +0100

    set up test environment for About page...

--- src/components/test.js
+++ src/components/test.js
@@ -1,24 +1,14 @@
 import { useContext } from 'react';
 import { CarContext } from '../contexts/CarContext';
 import { CartContext } from '../contexts/CartContext';
+import About from '../pages/About';
 const Test = () => {
   const { cars } = useContext(CarContext);
   const { addToCart, cart } = useContext(CartContext);
   console.log(cars);
   return (
     <div>
-      {/* for testing, display cart length */}
-      {/* <h2>cart items {cart.length}</h2> */}
-      {cars.map((car) => (
-        <div>
-          {/* for testing, click on title to add to cart */}
-          <h1 onClick={() => addToCart(car)}>{car.make}</h1>
-          <p>{car.make}</p>
-          <p>{car.model}</p>
-          <p>{car.year}</p>
-          <p>{`${car.discounted}`}</p>
-        </div>
-      ))}
+      <About />
     </div>
   );
 };
