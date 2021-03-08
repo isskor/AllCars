b2c5f782212372d3178910c58fece8380f1d89e6
Mehmet Ziya <mehmetziya82@gmail.com>
2021-03-08 12:38:34 +0100

    added css file for CartPage

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -2,12 +2,13 @@ import { useContext } from 'react';
 import { CartContext } from '../contexts/CartContext';
 import CartItem from '../components/CartItem';
 import CartForm from '../components/CartForm';
+import styles from '../css/CartPage.module.css' ;
 
 const CartPage = () => {
     const { cart } = useContext(CartContext);
     const totalCost = cart.reduce((acc, cur) => acc + cur.price, 0);
     return (
-        <div>
+        <div className={styles.cartContainer}>
             <ul>
                 {cart.map((cartItem) => (
                     <CartItem cartItem={cartItem} key={cartItem.vin} />
