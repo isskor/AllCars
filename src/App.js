f0dc6b66e3cd2b8ee81f00eaf6979d6201d8b3b2
jerry <jphuong91@gmail.com>
2021-03-09 15:02:13 +0100

    fixed some bugs in styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CarContextProvider from './contexts/CarContext';
 import CartContextProvider from './contexts/CartContext';
 import Home from './pages/Home';
 import About from './pages/About';
+import Deals from './pages/deals';
 function App() {
   return (
     <div className='App'>
@@ -12,7 +13,7 @@ function App() {
           <CartContextProvider>
             <Navbar />
             <Switch>
-              <Route path='/deals' />
+              <Route path='/deals' component={Deals} />
               <Route exact path='/' component={Home} />
               <Route path='/about' component={About} />
               <Route exact path='/car/:id' />
