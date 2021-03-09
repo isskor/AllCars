3a7c272696d996f78214c9154793c03ca8f5fdb3
jerry <jphuong91@gmail.com>
2021-03-09 14:34:06 +0100

    uncomment navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import Navbar from './components/Navbar';
+import Navbar from './components/Navbar';
 import CarContextProvider from './contexts/CarContext';
 import CartContextProvider from './contexts/CartContext';
 import Home from './pages/Home';
@@ -13,17 +13,13 @@ function App() {
             <Navbar />
 
             <Switch>
-                <Route path='/deals' />
-                <Route exact path='/' component={Home}/>
-                <Route path='/about' component={About}/>
-                <Route exact path='/car/:id' />
-                <Route exact path='/cart' />
-                <Route path='/receipt' />
+              <Route path='/deals' />
+              <Route exact path='/' component={Home} />
+              <Route path='/about' component={About} />
+              <Route exact path='/car/:id' />
+              <Route exact path='/cart' />
+              <Route path='/receipt' />
             </Switch>
-            
-            
-            
-
           </CartContextProvider>
         </CarContextProvider>
       </Router>
