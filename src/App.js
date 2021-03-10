a77f5a7b52b1adf5ac95d3943bef494ab08f43d8
jerry <jphuong91@gmail.com>
2021-03-10 13:27:25 +0100

    added page_container to wrap all pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/Navbar';
+// context
 import CarContextProvider from './contexts/CarContext';
 import CartContextProvider from './contexts/CartContext';
+// components
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Deals from './pages/deals';
 import CartPage from './pages/CartPage';
+// styles
+import styles from './css/App.module.css';
 function App() {
   return (
     <div className='App'>
@@ -13,14 +17,16 @@ function App() {
         <CarContextProvider>
           <CartContextProvider>
             <Navbar />
-            <Switch>
-              <Route path='/deals' component={Deals} />
-              <Route exact path='/' component={Home} />
-              <Route path='/about' component={About} />
-              <Route exact path='/car/:id' />
-              <Route exact path='/cart' component={CartPage} />
-              <Route path='/receipt' />
-            </Switch>
+            <div className={styles.page_container}>
+              <Switch>
+                <Route path='/deals' component={Deals} />
+                <Route exact path='/' component={Home} />
+                <Route path='/about' component={About} />
+                <Route exact path='/car/:id' />
+                <Route exact path='/cart' component={CartPage} />
+                <Route path='/receipt' />
+              </Switch>
+            </div>
           </CartContextProvider>
         </CarContextProvider>
       </Router>
