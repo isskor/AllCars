ad352229dce2143dc87382cf6fbf3d329f38bbaf
Pernilla <coolanilla@hotmail.com>
2021-03-10 12:28:16 +0100

    Added styling to About on breakpoint 768

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,10 +11,10 @@ const About = () => {
                 <h1 className={styles.aboutTitle}>About us</h1>
             </div>
             <div className={styles.aboutContentContainer}>
-                <div className={styles.aboutText}>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci veritatis cupiditate ipsa ducimus ex! Perspiciatis quisquam exercitationem debitis recusandae repellat magnam maxime sunt. Maxime consequuntur, at quidem eius rerum pariatur.</p>
-                </div>
-                <div className={styles.aboutImageStaff}>
+                <div className={styles.textAndImg}>
+                    <div className={styles.aboutText}>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci veritatis cupiditate ipsa ducimus ex! Perspiciatis quisquam exercitationem debitis recusandae repellat magnam maxime sunt. Maxime consequuntur, at quidem eius rerum pariatur.</p>
+                    </div>
                     <img className={styles.aboutTeam} src="/assets/background/allcars-team.jpg" alt="About the AllCars staff" />
                 </div>
                 <div className={styles.aboutContactInfo}>
@@ -22,13 +22,15 @@ const About = () => {
                         <p className={styles.allCarsCopyright}>AllCars &copy;</p>
                         <a className={styles.email} href="mailto: info@allcars.com">info@allcars.com</a>
                     </div>
-                    <div className={styles.contactAddress}>
-                        <p className={styles.contactUs}>Visit us at</p>
-                        <p>78 Shire Oak Road, Willerby</p>
-                    </div>
-                    <div className={styles.contactPhonenumber}>
-                        <p className={styles.contactUs}>Or call us on</p>
-                        <p>078 2006 6076</p>
+                    <div className={styles.addressAndPhonenumber}>
+                        <div className={styles.contactAddress}>
+                            <p className={styles.contactUs}>Visit us at</p>
+                            <p className={styles.contactText}>78 Shire Oak Road, Willerby</p>
+                        </div>
+                        <div className={styles.contactPhonenumber}>
+                            <p className={styles.contactUs}>Or call us on</p>
+                            <p className={styles.contactText}>078 2006 6076</p>
+                        </div>
                     </div>
                 </div>
             </div>
