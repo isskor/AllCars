7b49610e0f490acac73a42ce4f8ac87fbc2b4953
jerry <jphuong91@gmail.com>
2021-03-16 13:22:36 +0100

    created loginRegisterPage

---
+++ src/pages/loginRegisterPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+function loginRegisterPage() {
+  const history = useHistory();
+
+  return <div></div>;
+}
+
+export default loginRegisterPage;
