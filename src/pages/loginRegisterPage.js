bc2b93cadd7fd784f0fdce4f241a14cfb621ede7
jerry <jphuong91@gmail.com>
2021-03-16 14:05:44 +0100

    created loginComponent, and added styles

--- src/pages/loginRegisterPage.js
+++ src/pages/loginRegisterPage.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import LoginForm from '../components/LoginForm';
+import styles from '../css/LoginRegister.module.css';
 
-function loginRegisterPage() {
+function LoginRegisterPage() {
   const history = useHistory();
 
-  return <div></div>;
+  return (
+    <div className={styles.log_reg_container}>
+      <LoginForm />
+    </div>
+  );
 }
 
-export default loginRegisterPage;
+export default LoginRegisterPage;
