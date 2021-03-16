547c384d384d2b6d146088a004cbb9b42aeb2143
jerry <jphuong91@gmail.com>
2021-03-16 14:40:07 +0100

    created register component, + styling, added to Log_reg_page fixed route

---
+++ src/components/RegisterForm.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from '../css/LoginForm.module.css';
+function RegisterForm() {
+  const form = [
+    { name: 'Email', type: 'email', placeholder: 'Please enter your email' },
+    {
+      name: 'Password',
+      type: 'password',
+      placeholder: 'Please enter your password',
+    },
+    {
+      name: 'Confirm Password',
+      type: 'password',
+      placeholder: 'Please confirm your password',
+    },
+    {
+      name: 'Address',
+      type: 'text',
+      placeholder: 'Enter your address',
+    },
+    {
+      name: 'Phone Number',
+      type: 'text',
+      placeholder: 'Please enter your phone number',
+    },
+  ];
+
+  return (
+    <div className={styles.loginForm}>
+      <form>
+        <h1>Register</h1>
+        <p>
+          <Link to='/login'>Already have an Account? Login!</Link>
+        </p>
+        {form?.map((item) => (
+          <div className={styles.form_group} key={item.name}>
+            <label className={styles.login_label}>{item.name}</label>
+            <input
+              type={item.type}
+              name={item.name}
+              placeholder={item.placeholder}
+            />
+          </div>
+        ))}
+
+        <div className={styles.form_group}>
+          <button type='button'>Register</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default RegisterForm;
--- src/pages/loginRegisterPage.js
+++ src/pages/loginRegisterPage.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import LoginForm from '../components/LoginForm';
+import RegisterForm from '../components/RegisterForm';
 import styles from '../css/LoginRegister.module.css';
 
 function LoginRegisterPage() {
   const history = useHistory();
-
+  const { pathname } = history.location;
   return (
     <div className={styles.log_reg_container}>
-      <LoginForm />
+      <div className={styles.log_reg_wrapper}>
+        {pathname === '/login' && <LoginForm />}
+        {pathname === '/register' && <RegisterForm />}
+        <h3>A new Adventure Awaits!</h3>
+      </div>
     </div>
   );
 }
