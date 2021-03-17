39d0bda98065d43051d11bfa66a18249b4923678
Pernilla <coolanilla@hotmail.com>
2021-03-17 15:15:21 +0100

    Created popup component

---
+++ src/components/ConfirmationPopup.js
@@ -0,0 +1,9 @@
+function ConfirmationPopup() {
+    return (
+        <div className="popup">
+            <p>You are now registered! Please log in.</p>
+        </div>
+    )
+}
+
+export default ConfirmationPopup;
