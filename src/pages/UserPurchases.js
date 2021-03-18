2eced99e7fb19885cd35fb1f5705fc179b84d4a0
Johannes <polyspark.prod@gmail.com>
2021-03-18 14:05:00 +0100

    Added header to userprofile page

--- src/pages/UserPurchases.js
+++ src/pages/UserPurchases.js
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import Banner from '../components/Banner';
 import { UserContext } from '../contexts/UserContext';
 function UserPurchases() {
   const { currentUser, setCurrentUser } = useContext(UserContext);
@@ -6,7 +7,15 @@ function UserPurchases() {
 
   return (
     <div>
+      <Banner 
+        header={"My page"}
+        imgSrc={"/assets/background/allcars_about.jpg"}
+        altText={"My page image"}
+      />
       <button onClick={() => setCurrentUser(null)}>logout</button>
+
+
+
     </div>
   );
 }
