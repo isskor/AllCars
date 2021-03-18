36ab8788b0d37687a5416bccdd524c49748bfdc6
Johannes <polyspark.prod@gmail.com>
2021-03-18 14:30:43 +0100

    Working on order card

--- src/components/OrderCard.js
+++ src/components/OrderCard.js
@@ -1,13 +1,26 @@
 const OrderCard = () => {
   return (
     <div className="OrderCardContainer">
-      <h2>Order: #11111</h2>
-      <p>Time: 10:00</p>
-      <p>Price: *Total price here*</p>
-      <p>Order ID: 123 456 789</p>
-      <div className="listOfCarsBought">
-        <li>carName1</li>
-        <li>carName2</li>
+      <div className="order">
+        <h2>Order: #1</h2>
+        <p>Time: 10:00</p>
+        <p>Price: *Total price here*</p>
+        <p>Order ID: 123 456 789</p>
+        <div className="listOfCarsBought">
+          {/* Loop out reciept here */}
+          <li>carName1</li>
+          <li>carName2</li>
+        </div>
+      </div>
+      <div className="order">
+        <h2>Order: #1</h2>
+        <p>Time: 10:00</p>
+        <p>Price: *Total price here*</p>
+        <p>Order ID: 123 456 789</p>
+        <div className="listOfCarsBought">
+          <li>carName1</li>
+          <li>carName2</li>
+        </div>
       </div>
     </div>
   );
