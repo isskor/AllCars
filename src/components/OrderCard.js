97be93159e74aff78aa54265fbe9c323a9a0e03b
Johannes <polyspark.prod@gmail.com>
2021-03-18 14:21:23 +0100

    Adding OrderCard

---
+++ src/components/OrderCard.js
@@ -0,0 +1,16 @@
+const OrderCard = () => {
+  return (
+    <div className="OrderCardContainer">
+      <h2>Order: #11111</h2>
+      <p>Time: 10:00</p>
+      <p>Price: *Total price here*</p>
+      <p>Order ID: 123 456 789</p>
+      <div className="listOfCarsBought">
+        <li>carName1</li>
+        <li>carName2</li>
+      </div>
+    </div>
+  );
+}
+ 
+export default OrderCard;
--- src/pages/UserPurchases.js
+++ src/pages/UserPurchases.js
@@ -1,5 +1,7 @@
+import { OrderedListOutlined } from '@ant-design/icons';
 import { useContext } from 'react';
 import Banner from '../components/Banner';
+import OrderCard from '../components/OrderCard';
 import { UserContext } from '../contexts/UserContext';
 function UserPurchases() {
   const { currentUser, setCurrentUser } = useContext(UserContext);
@@ -14,7 +16,11 @@ function UserPurchases() {
       />
       <button onClick={() => setCurrentUser(null)}>logout</button>
 
+      <div className="myPurchasesContainer">
+        {/* map out all orders connected to profile here. */}
+        <OrderCard/>
 
+      </div>
 
     </div>
   );
