315379453fa595c164dee9f10b48f59593705ce6
Johannes <polyspark.prod@gmail.com>
2021-03-19 10:39:18 +0100

    Needs to be linked up to purchase history

--- src/components/OrderCard.js
+++ src/components/OrderCard.js
@@ -1,29 +1,36 @@
+import styles from '../css/CartItem.module.css'
+
 const OrderCard = () => {
-  return (
-    <div className="OrderCardContainer">
-      <div className="order">
-        <h2>Order: #1</h2>
-        <p>Time: 10:00</p>
-        <p>Price: *Total price here*</p>
-        <p>Order ID: 123 456 789</p>
-        <div className="listOfCarsBought">
-          {/* Loop out reciept here */}
-          <li>carName1</li>
-          <li>carName2</li>
+  let car = {
+    make: "Ford",
+    model: 'Econoline',
+    year: "2002",
+    miles: 12550,
+    price: 150
+  }
+    return ( 
+        <div className={styles.carCard}>
+            <div className={styles.img_container}>
+                <img
+                    src={`/assets/car-pictures/${car.make}-${car.model}-${car.year}.jpg`}
+                    alt={`${car.make}-${car.model}`}
+                />
+            </div>
+            <div className={styles.car_info_container}>
+                <div className='car_info'>         
+                    <p> Model: {car.make} {car.model} </p>
+                    <p> Year: {car.year}</p>
+                </div>
+                <div className='car_miles_ctn'>
+                    
+                    <p className='car_miles'> Miles: {car.miles}</p>
+                </div>
+                <div className='car_price_ctn'>
+                     <p className={styles.car_price}> Price: $ {car.price.toLocaleString()} </p>
+                </div>
+            </div>
         </div>
-      </div>
-      <div className="order">
-        <h2>Order: #1</h2>
-        <p>Time: 10:00</p>
-        <p>Price: *Total price here*</p>
-        <p>Order ID: 123 456 789</p>
-        <div className="listOfCarsBought">
-          <li>carName1</li>
-          <li>carName2</li>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
  
 export default OrderCard;
