0cf849b57847934cdbb1944542d5c6add786d009
Johannes <polyspark.prod@gmail.com>
2021-03-19 12:03:20 +0100

    Styling OrderCard

--- src/components/OrderCard.js
+++ src/components/OrderCard.js
@@ -4,9 +4,10 @@ import OrderCardOrders from './OrderCardOrders';
 const OrderCard = () => {
   return(
   <div className={style.orderCardContainer}>
+    <h2>Order: <span>#123</span></h2>
     <div className={style.info}>
-      <h2>Order: <span>#420</span></h2>
       <p>Total price: <span>40000</span></p>
+      <p>ID: <span>1236 231 SD58 </span></p>
       <p>
         <span className="date">10.10.2010</span> - <span className="time">10:00</span>
       </p>
