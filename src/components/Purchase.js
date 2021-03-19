23a34adcf9000f1d6139a01401eba199e463e695
Pernilla <coolanilla@hotmail.com>
2021-03-19 10:21:37 +0100

    Created purchase component with header and div

---
+++ src/components/Purchase.js
@@ -0,0 +1,14 @@
+function Purchase() {
+
+    return (
+        <div className="container">
+            <h2>My purchases</h2>
+            <div className="orderCards">
+                
+            </div>
+        </div>
+    )
+
+}
+
+export default Purchase;
