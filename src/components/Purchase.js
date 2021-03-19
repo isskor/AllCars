ec858786f8c6eddc1824fd8cd9b9f43f795a7710
Pernilla <coolanilla@hotmail.com>
2021-03-19 10:29:07 +0100

    Added purchase component to userpage and order card to purchase

--- src/components/Purchase.js
+++ src/components/Purchase.js
@@ -1,10 +1,13 @@
+import OrderCard from './OrderCard';
+
 function Purchase() {
 
     return (
         <div className="container">
             <h2>My purchases</h2>
             <div className="orderCards">
-                
+                {/* map out OrderCards here */}
+                <OrderCard />
             </div>
         </div>
     )
--- src/pages/UserPurchases.js
+++ src/pages/UserPurchases.js
@@ -1,7 +1,7 @@
 import { OrderedListOutlined } from '@ant-design/icons';
 import { useContext } from 'react';
 import Banner from '../components/Banner';
-import OrderCard from '../components/OrderCard';
+import Purchase from '../components/Purchase';
 import { UserContext } from '../contexts/UserContext';
 function UserPurchases() {
   const { currentUser, setCurrentUser } = useContext(UserContext);
@@ -16,11 +16,7 @@ function UserPurchases() {
       />
       <button onClick={() => setCurrentUser(null)}>logout</button>
 
-      <div className="myPurchasesContainer">
-        {/* map out all orders connected to profile here. */}
-        <OrderCard/>
-
-      </div>
+      <Purchase />
 
     </div>
   );
