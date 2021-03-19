1a58b0def20c0dd3a94e65bf67d09f2a3d09cfd3
Pernilla <coolanilla@hotmail.com>
2021-03-19 10:44:58 +0100

    Added styling on purchase component

--- src/components/Purchase.js
+++ src/components/Purchase.js
@@ -1,10 +1,11 @@
+import style from '../css/Purchase.module.css';
 import OrderCard from './OrderCard';
 
 function Purchase() {
 
     return (
-        <div className="container">
-            <h2>My purchases</h2>
+        <div className={style.purchaseContainer}>
+            <h2 className={style.purchaseHeader}>My purchases</h2>
             <div className="orderCards">
                 {/* map out OrderCards here */}
                 <OrderCard />
