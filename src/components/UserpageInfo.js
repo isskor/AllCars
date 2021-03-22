fe210d0d27b534069d694d5b66e38d4aeef9379d
Johannes <polyspark.prod@gmail.com>
2021-03-22 09:47:59 +0100

    Linked up userInfo to UserContext

--- src/components/UserpageInfo.js
+++ src/components/UserpageInfo.js
@@ -1,11 +1,18 @@
 import style from '../css/UserInfo.module.css';
+import { useContext } from 'react';
+import { UserContext } from '../contexts/UserContext';
 
-const UserPageInfo = () => {
+  const UserPageInfo = () => {
+
+  const { currentUser } = useContext(UserContext);
+  const user = currentUser;
+  console.log(user);
   return (
     <div className={style.userInfo}>
-        <p>Name: <span>Bob Bobbsen</span></p>
-        <p>Adress: <span>Bobstreet 808, 8080 Bobland </span></p>
-        <p>Email: <span>Bob@bob.se</span></p>
+        <p>Name: <span>{user.username}</span></p>
+        <p>Email: <span>{user.username}</span></p>
+        <p>Address: <span>{user.address}</span></p>
+        <p>Phone: <span>{user.phone_number}</span></p>
         {/* Add all info connected to profile here */}
       </div>
   );
