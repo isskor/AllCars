3feca8f0eae6f8b8817ef5e397e18d2733fb7800
jerry <jphuong91@gmail.com>
2021-03-22 14:35:08 +0100

    fix conflict

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -23,19 +23,11 @@ function UserPage() {
         <div className={styles.purchases}>
           <h2 className={styles.purchasesHeading}>My Purchases</h2>
           <ul>
-            {currentUser?.purchaseHistory.map((order) => (
+            {currentUser?.purchaseHistory?.map((order) => (
               <OrderCard order={order} key={order.id} />
             ))}
           </ul>
         </div>
-      </div>
-      <div className={styles.purchases}>
-        <h2>My Purchases</h2>
-        <ul>
-          {currentUser?.purchaseHistory?.map((order) => (
-            <OrderCard order={order} key={order.id} />
-          ))}
-        </ul>
       </div>
     </div>
   );
